6759b6837e12b824091108b82e012e47b27ac5c4
kennedy788 <salvadorjohnkennedy78@gmail.com>
2024-10-19 21:24:37 +0800

    Create arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,9 @@
+let favSingers = ["Billie Eilish", "Taylor Swift", "Snoop Dogg"];
+ console.log(favSingers[0]);
+
+
+let favNumbers = [10, 13, 28];
+console.log(favNumbers);
+
+let mixedArr = ["string", ["otherarray"], 123, true];
+console.log(mixedArr);
